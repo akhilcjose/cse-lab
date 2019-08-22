98d2c913a3349ef768994e9472663ddc543c074b
akhilcjose <53758418+akhilcjose@users.noreply.github.com>
2019-08-22 12:25:12 +0530

    Add files via upload

--- linkedlist.c
+++ linkedlist.c
@@ -5,9 +5,10 @@ struct node{
            };
 void insertion(struct node* head,int x,int dat){
 {
- int count=0;
+ int count=1;
  struct node*newnode; 
  newnode=(struct node*)malloc(sizeof(struct node));
+ newnode->data=dat;
  if(newnode==NULL)
  {
   printf("no space left")
@@ -22,17 +23,48 @@ void insertion(struct node* head,int x,int dat){
  temp=head;
  while(temp->link!=NULL)
  {
-  count=count+1;
   if(count==x)
   {
+   if(count =1)
+    {
+     newnode->link=temp;
+     head=newnode;
+     break;
+     }
    newnode->link=temp->link;
    temp->link=newnode;
+   break;
   }
   temp=temp->link;
+  count=count+1;
  }
  if(count<x)
  {
   printf("pos invalid")
- } 
+ }
+}
+void deletion(struct node* head,int pos)
+{
+}
+void searching(struct node* head,int data)
+{
+ int count=0;
+ struct node* temp;
+ temp=head;
+ while(temp->link!=NULL)
+ {
+  count=count+1;
+  if(temp->data==data)
+  {
+   printf();
+  }
+  temp=temp->link;
+ }
+}
+void traversal()
+{
+}
+
+  
   
   
