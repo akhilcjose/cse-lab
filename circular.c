7ce9111c57bd1fce670324ceee3b32bd027b9cfd
akhilcjose <53758418+akhilcjose@users.noreply.github.com>
2019-10-14 02:55:37 +0700

    Add files via upload

--- circular.c
+++ circular.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#define max 10
+#define max 5
 int front=-1;
 int rear=-1;
 int arr[max];
@@ -7,66 +7,76 @@ void insert(int data)
 {
  if(front==0 && rear==max-1||front==rear+1)
  {
-  printf("queue is full");
+  printf("queue is full\n");
   }
  else
  {
-   if(front=-1)
+   if(front==-1)
     {
      front=0;
      rear=0;
+     arr[rear]=data;
     }
    else
-    if(rear=max-1)
-     {
-      rear=0;
-     }
-    else
-     {
-      rear=rear+1;
-     }
-   arr[rear]=data;
+    rear=(rear+1)%max;
+    arr[rear]=data;
  }
- 
+
 }
 void delete()
 {
+ int q;
  if(front==-1 )
  {
-  printf("queue is empty");
+  printf("queue is empty\n");
   }
  else
  {
-  if(front=rear)
+  q=arr[front];
+  if(front==rear)
   {
-   printf("deleted element is %d",arr[front]);
-   front=rear=-1;
-   }
-  else 
-   if(front = max-1)
-   {
-    printf("deleted element is %d",arr[front]);
-    front=0;
-    }
-    else
-    {
-     printf("deleted element is %d",arr[front]);
-     front=front+1;
-    }
+      front=rear=-1;
+  }
+  else
+  {
+      front=(front+1)%max;
+  }
+  printf("deleted element is %d",q);
  }
 }
 void display()
 {
+ if(front==-1||rear==-1)
+ {
+     printf("queue is empty");
+ }
+ else
+ {
+    if(front<rear)
+    {
+        for(int i=front;i<=rear;i++)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    else if(rear<front)
+    {
+        for(int j=front;j<=max;j++)
+            printf("%d",arr[j]);
+        for(int k=0;k<rear;k++)
+            printf("%d",arr[k]);
+    }
+ }
 }
 int main(void)
-{ 
+{
  int ch=0,dat;
  while(ch!=4)
  {
   printf("\n1.insert\n2.delete\n3.display\n4.exit\nenter choice :");
   scanf("%d",&ch);
   switch(ch)
-   { 
+   {
     case 1:printf("\nenter data to be pushed: ");
            scanf("%d",&dat);
            insert(dat);
